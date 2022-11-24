614914acdc0f0e9fd8d6ff41437a4c8686910831
arfin-arif <arifmtbd@gmail.com>
2022-11-25 04:09:47 +0600

    product catd button added

--- src/Pages/CategoryWiseProducts/CategoryWiseProducts.js
+++ src/Pages/CategoryWiseProducts/CategoryWiseProducts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaMapMarkerAlt, FaPhone, FaPhoneAlt } from 'react-icons/fa';
+import { FaClock, FaMapMarkerAlt, FaPhone, FaPhoneAlt, FaUserAlt } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 
 const CategoryWiseProducts = () => {
@@ -16,9 +16,9 @@ const CategoryWiseProducts = () => {
                     <div class="w-full max-w-sm overflow-hidden rounded-lg bg-base-100 shadow-lg">
                         <img class="object-cover object-center w-full h-56" src={product.image} alt="avatar" />
 
-                        <div class="flex items-center px-6 py-3">
+                        {/* <div class="flex items-center px-6 py-3">
                             <h1 class="mx-3 text-lg font-semibold  ">$ {product.price}</h1>
-                        </div>
+                        </div> */}
 
                         <div class="px-6 py-4">
                             <h1 class="text-xl font-semibold  ">{product.name} </h1>
@@ -34,23 +34,45 @@ const CategoryWiseProducts = () => {
                                     Years of Use : {product.yearsOfUse}
                                 </div>
                             </div>
-                            <div class="flex items-center mt-4    ">
-                                <FaMapMarkerAlt></FaMapMarkerAlt>
-                                <h1 class="px-2 text-sm">{product.location} </h1>
+                            <div class="flex justify-between mt-4    ">
+                                <div className="flex">
+                                    <FaMapMarkerAlt></FaMapMarkerAlt>
+                                    <h1 class="px-2 text-sm">{product.location} </h1>
+
+                                </div>
+                                <div className="flex">
+
+                                    <FaUserAlt></FaUserAlt>
+                                    <h1 class="px-2 text-sm">{product.seller}</h1>
+
+                                </div>
+
+
                             </div>
 
-                            <div class="flex items-center mt-4    ">
-                                <FaPhoneAlt></FaPhoneAlt>
+                            <div class="flex justify-between mt-4    ">
+                                <div className="flex">
+                                    <FaPhoneAlt></FaPhoneAlt>
 
-                                <h1 class="px-2 text-sm">{product.phone}</h1>
+                                    <h1 class="px-2 text-sm">{product.phone}</h1>
+                                </div>
+                                <div className="flex">
+                                    <FaClock></FaClock>
+                                    <h1 class="px-2 text-sm">{product.dateOfPost.slice(0, 10)}</h1>
+                                </div>
                             </div>
 
-                            <div class="flex items-center mt-4    ">
-                                <svg aria-label="email icon" class="w-6 h-6 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.00977 5.83789C3.00977 5.28561 3.45748 4.83789 4.00977 4.83789H20C20.5523 4.83789 21 5.28561 21 5.83789V17.1621C21 18.2667 20.1046 19.1621 19 19.1621H5C3.89543 19.1621 3 18.2667 3 17.1621V6.16211C3 6.11449 3.00333 6.06765 3.00977 6.0218V5.83789ZM5 8.06165V17.1621H19V8.06199L14.1215 12.9405C12.9499 14.1121 11.0504 14.1121 9.87885 12.9405L5 8.06165ZM6.57232 6.80554H17.428L12.7073 11.5263C12.3168 11.9168 11.6836 11.9168 11.2931 11.5263L6.57232 6.80554Z" />
-                                </svg>
 
-                                <h1 class="px-2 text-sm">patterson@example.com</h1>
+                        </div>
+                        <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto ">
+
+                            <div class="w-56 mt-4 overflow-hidden bg-sky-300 rounded-lg shadow-lg md:w-64">
+                                <h3 class="py-2 font-bold tracking-wide text-center uppercase dark:text-white">Nike Revolt</h3>
+
+                                <div class="flex items-center justify-between px-3 py-2">
+                                    <span class="font-bold">${product.price}</span>
+                                    <button class="px-2 btn btn-sm btn-success py-1 text-xs font-semibold uppercase ">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>)
