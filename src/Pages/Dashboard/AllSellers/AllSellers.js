d4e6d68bd7977969a56057516b3ec865eef34794
arfin-arif <arifmtbd@gmail.com>
2022-11-25 23:53:26 +0600

    all seller page created

--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,22 +1,61 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllSellers = () => {
-    const { data: products = [], refetch, isLoading } = useQuery({
-        queryKey: ['products'],
+    const { data: sellers = [], refetch, isLoading } = useQuery({
+        queryKey: ['sellers'],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/allsellers`);
-
+            const res = await fetch(`http://localhost:5000/allsellers?role=Seller`);
             const data = await res.json();
             return data;
         }
 
     })
 
-    return (
-        <div>
-            This is all seller
+    const handleDeleteProduct = (id) => {
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE',
+        }).then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('Successfully Deleted')
+                    refetch()
+                }
+            })
 
+    }
+
+    return (
+        <div className="overflow-x-auto mb-5">
+
+            <table className="table w-full">
+
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        sellers?.map((seller, index) => <tr key={seller._id} className='hover' >
+                            <th>{index + 1}</th>
+                            <td>{seller.name}</td>
+                            <td>{seller.email} </td>
+                            <td>{seller.role ? seller.role : <p>Admin</p>} </td>
+                            <td ><label onClick={() => handleDeleteProduct(seller._id)} className='btn btn-sm' htmlFor="">X</label></td>
+
+                        </tr>)
+
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -17,19 +17,13 @@ const Navbar = () => {
             <li><Link to='/about'>About</Link></li>
             {user?.uid ?
                 <>
-                    <div>
-                        <div className="dropdown dropdown-end">
-                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10 rounded-full">
-                                    <img src="https://placeimg.com/80/80/people" />
-                                </div>
-                            </label>
-                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><Link to='/dashboard'>Dashboard</Link></li>
-                                <li onClick={handleLogOut}><Link to='/login'>Sign Out</Link></li>
-                            </ul>
-                        </div>
-                    </div>
+                    <>
+
+
+                        <li><Link to='/dashboard'>Dashboard</Link></li>
+                        <li onClick={handleLogOut}><Link to='/login'>Sign Out</Link></li>
+
+                    </>
 
                 </>
                 :
